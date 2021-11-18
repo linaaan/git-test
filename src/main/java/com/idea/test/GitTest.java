f744ab0337ade8db5b4983573995402528f98ab8
linaaan <807605729@qq.com>
2021-11-18 14:56:27 +0800

    hot-fix commit2

--- src/main/java/com/idea/test/GitTest.java
+++ src/main/java/com/idea/test/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hello hot-fix");
 
         System.out.println("就是要3git");
     }
